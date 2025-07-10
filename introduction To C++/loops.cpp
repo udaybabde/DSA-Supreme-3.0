ba89b493a1e39aaed7c2a18f217767cd241a8869
udaybabde <udaybabde@gmail.com>
2025-07-11 00:07:28 +0530

    loops code file commited

---
+++ introduction To C++/loops.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //for loop for my name
+    // for(int count=1;count<=10;count=count+1){
+    //     cout<<"uday"<<" "<<endl;
+    // }
+
+   
+   //counting 1-100 without initialization updation and condition in for loop
+    // int i=1;
+    // for(;;){
+    //     cout<<i<<" ";
+    //     i++;
+    //     if(i==101)
+    //         break;
+        
+    // }
+
+    //break keyword case 1    output:1 2 3 4 5
+    // for(int i=1;i<=10;i=i+1){
+    //     cout<<i<<" ";
+
+    //     if(i==5){
+    //         break;
+    //     }
+    // }
+
+
+   //break keyword case 2   output:1 2 3 4
+    // for(int i=1;i<=10;i=i+1){
+    //   if(i==5){
+    //         break;
+    //     }
+    //        cout<<i<<" ";
+    // }
+
+
+    //continue keyword
+    // for(int i=1;i<=10;i=i+1){
+    //     if(i==5){
+    //         continue;
+    //     }
+    //       cout<<i<<" ";
+
+    // }
+
+
+    //while loop
+    // int i=1;
+    // while(i<=10){
+    //     cout<<i<<" ";
+    //     i=i+1;
+    // }
+
+
+    //homework question
+
+    //1-100
+    // for(int i=1;i<=100;i=i+1){
+    //     cout<<i<<" ";
+    // }
+
+
+//7 table
+// int j=7;
+// for(int i=1;i<=10;i=i+1){
+//     cout<<j*i<<" ";
+// }
+
+
+//A-Z
+// for(char i='A';i<='Z';i=i+1){
+//     cout<<i<<" ";
+// }
+
+
+//a-z
+// for(char i='a';i<='z';i=i+1){
+//     cout<<i<<" ";
+// }
+
+
+// 1 to -10
+// for(int i=1;i>=-10;i=i-1){
+//     cout<<i<<" ";
+// }
+
+
+// 100 to 1
+// for(int i=100;i>=1;i=i-1){
+//     cout<<i<<" ";
+// }
+
+
+//using while loop(100-1)
+// int i=100;
+// while(i>=1){
+//     cout<<i<<" ";
+//     i=i-1;
+// }
+
+
+//do while loop (20 times uday)
+// int count=20;
+
+// int i=1;
+// do{
+//     cout<<"uday"<<" ";
+//     i=i+1;
+//     }while(i<=count);
+
+
+// for(int i=1;i<=5;i=i+1);
+// {
+//     cout<<i<<" ";
+// }
+
+
+
+    return 0;
+}
