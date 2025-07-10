bfb5285fc2075b07871f0f92ef76242f7b869d93
udaybabde <udaybabde@gmail.com>
2025-07-11 00:03:40 +0530

    control flow code file commited

---
+++ introduction To C++/controlflow.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+//int main(){
+
+//nested if
+//     int weight;
+//     cout<<"your weight is:";
+//     cin>>weight;
+    
+//    int height;
+//     cout<<"your height is:";
+//     cin>>height;
+
+//     if(height>170){
+        
+//         if(weight>70)
+//         cout<<"uday"<<endl;
+
+//         else
+//         {
+//             cout<<"Aniket"<<endl;
+//         }
+//     }
+//     else{
+//         cout<<"harsh"<<endl;
+//     }
+
+
+
+
+
+
+
+//if else if
+int main(){
+
+    int marks;
+    cout<<"enter your marks :: ";
+    cin>>marks;
+
+    if(marks>90){
+        cout<<"A"<<endl;
+    }
+    else if(marks>80){
+        cout<<"B"<<endl;
+    }
+    else if(marks>70){
+        cout<<"C"<<endl;
+    }
+
+    else{
+        cout<<"padhai karo beta";
+    }
+
+
+
+
+    return 0;
+
+}
